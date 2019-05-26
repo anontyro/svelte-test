4dd641e6544d60777ec95d161c5484d64ecbe435
Alexander Wilkinson <alexwilkinson@gmail.com>
2019-05-26 16:16:28 +0800

    adding in basic use to input

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+  import UserInput from "./components/chat/UserInput.svelte";
+  export let name;
+</script>
+
+<style>
+  h1 {
+    color: purple;
+  }
+</style>
+
+<h1>Hello {name}!</h1>
+<UserInput />
---
+++ src/components/chat/UserInput.svelte
@@ -0,0 +1,36 @@
+<script>
+  const defaultText = "Enter text to start talking";
+  let userData = defaultText;
+
+  const onInputSelect = event => {
+    if (event.target.value === defaultText) {
+      userData = "";
+    }
+  };
+
+  const onInputBlur = event => {
+    if (event.target.value === "") {
+      userData = defaultText;
+    }
+  };
+</script>
+
+<style>
+  .user-input {
+    display: flex;
+    flex-direction: row;
+  }
+  textarea {
+    flex-grow: 1;
+  }
+</style>
+
+<div class="user-input">
+  <textarea
+    bind:value={userData}
+    on:click={onInputSelect}
+    on:blur={onInputBlur}
+    rows="3"
+    cols="50" />
+  <button>Submit</button>
+</div>
